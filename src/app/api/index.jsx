7e0052ae8f6383e004104ea67007ad8d8b51cc23
Jeffrey N. Esma <abd.kgd@gmail.com>
2021-03-09 01:06:46 +0800

    Update

--- src/app/api/index.jsx
+++ src/app/api/index.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const authURL = "http://localhost:7000/api/users/userpass";
-const accountURL = "http://localhost:7000/api/users"
-
 export const getAuth = () => axios.get(authURL);
+
+const accountURL = "http://localhost:7000/api/users"
 export const addAccount = (account) => axios.post(accountURL, account); 
